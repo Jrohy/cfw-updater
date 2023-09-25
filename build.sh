0feee7c2d8d061407295c59ab0a200f2730fbf26
Jrohy <euvkzx@gmail.com>
2023-09-25 22:33:15 +0800

    优化编译脚本

--- build.sh
+++ build.sh
@@ -1,76 +1,76 @@
 #!/bin/bash
 
-GITHUB_TOKEN=""
+github_token=""
 
-PROJECT="Jrohy/cfw-updater"
+project="Jrohy/cfw-updater"
 
 #获取当前的这个脚本所在绝对路径
-SHELL_PATH=$(cd `dirname $0`; pwd)
+shell_path=$(cd `dirname $0`; pwd)
 
 export CGO_ENABLED=1
 
 function uploadfile() {
-    FILE=$1
+    file=$1
 
-    CTYPE=$(file -b --mime-type $FILE)
+    ctype=$(file -b --mime-type $file)
 
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: ${CTYPE}" --data-binary @$FILE "https://uploads.github.com/repos/$PROJECT/releases/${RELEASE_ID}/assets?name=$(basename $FILE)"
+    curl -H "Authorization: token ${github_token}" -H "Content-Type: ${ctype}" --data-binary @$file "https://uploads.github.com/repos/$project/releases/${release_id}/assets?name=$(basename $file)"
 
     echo ""
 }
 
 function upload() {
-    FILE=$1
-    DGST=$1.dgst
-    openssl dgst -md5 $FILE | sed 's/([^)]*)//g' >> $DGST
-    openssl dgst -sha1 $FILE | sed 's/([^)]*)//g' >> $DGST
-    openssl dgst -sha256 $FILE | sed 's/([^)]*)//g' >> $DGST
-    openssl dgst -sha512 $FILE | sed 's/([^)]*)//g' >> $DGST
-    uploadfile $FILE
-    uploadfile $DGST
+    file=$1
+    dgst=$1.dgst
+    openssl dgst -md5 $file | sed 's/([^)]*)//g' >> $dgst
+    openssl dgst -sha1 $file | sed 's/([^)]*)//g' >> $dgst
+    openssl dgst -sha256 $file | sed 's/([^)]*)//g' >> $dgst
+    openssl dgst -sha512 $file | sed 's/([^)]*)//g' >> $dgst
+    uploadfile $file
+    uploadfile $dgst
 }
 
 [[ -z `command -v goversioninfo` ]] && go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo@latest
 
-VERSION=`git describe --tags $(git rev-list --tags --max-count=1)`
-NOW=`date "+%Y%m%d-%H%M"`
-GO_VERSION=`go version|awk '{print $3,$4}'`
-GIT_VERSION=`git rev-parse HEAD`
-LDFLAGS="-w -s -X 'main.version=$VERSION' -X 'main.buildDate=$NOW' -X 'main.goVersion=$GO_VERSION' -X 'main.gitVersion=$GIT_VERSION'"
+version=`git describe --tags $(git rev-list --tags --max-count=1)`
+now=`date "+%Y%m%d-%H%M"`
+go_version=`go version|awk '{print $3,$4}'`
+git_version=`git rev-parse HEAD`
+ldflags="-w -s -X 'main.version=$version' -X 'main.buildDate=$now' -X 'main.goversion=$go_version' -X 'main.gitversion=$git_version'"
 
-V=`echo "$VERSION"|sed 's/v//g'`
-PATCH_VERSION=`echo $V|cut -d . -f3`
-[[ -z $PATCH_VERSION ]] && PATCH_VERSION=-1
-EXE_VERSION_INFO="-product-version $VERSION -ver-major `echo $V|cut -d . -f1` -ver-minor `echo $V|cut -d . -f2` -ver-patch $PATCH_VERSION"
-goversioninfo -skip-versioninfo -64 -icon *.ico -copyright "Copyright © 2022 Jrohy" -product-version $VERSION -product-name "cfw-updater" -description "Clash for Windows便携版更新工具" $EXE_VERSION_INFO
-GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -ldflags "$LDFLAGS" -o result/cfw-updater.exe .
+v=`echo "$version"|sed 's/v//g'`
+patch_version=`echo $v|cut -d . -f3`
+[[ -z $patch_version ]] && patch_version=-1
+exe_version_info="-product-version $version -ver-major `echo $v|cut -d . -f1` -ver-minor `echo $v|cut -d . -f2` -ver-patch $patch_version"
+goversioninfo -skip-versioninfo -64 -icon *.ico -copyright "Copyright © 2022 Jrohy" -product-version $version -product-name "cfw-updater" -description "Clash for Windows便携版更新工具" $exe_version_info
+GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -ldflags "$ldflags" -o result/cfw-updater.exe .
 rm -f resource.syso
 
 if [[ `uname` == "Darwin" ]];then
-    GOOS=darwin GOARCH=arm64 go build -ldflags "$LDFLAGS" -o result/cfw-updater_mac_arm64 .
-    GOOS=darwin GOARCH=amd64 go build -ldflags "$LDFLAGS" -o result/cfw-updater_mac_amd64 .
+    GOOS=darwin GOARCH=arm64 go build -ldflags "$ldflags" -o result/cfw-updater_mac_arm64 .
+    GOOS=darwin GOARCH=amd64 go build -ldflags "$ldflags" -o result/cfw-updater_mac_amd64 .
 fi
 
 if [[ $# == 0 ]];then
 
     cd result
 
-    UPLOAD_ITEM=($(ls -l|awk '{print $9}'|xargs -r))
+    upload_item=($(ls -l|awk '{print $9}'|xargs -r))
 
-    curl -X POST -H "Authorization: token ${GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$PROJECT/releases -d '{"tag_name":"'$VERSION'", "name":"'$VERSION'"}'
+    curl -X POST -H "Authorization: token ${github_token}" -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$project/releases -d '{"tag_name":"'$version'", "name":"'$version'"}'
 
 	sleep 2
 
-	RELEASE_ID=`curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/$PROJECT/releases/latest|grep id|awk 'NR==1{print $2}'|sed 's/,//'`
+	release_id=`curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/$project/releases/latest|grep id|awk 'NR==1{print $2}'|sed 's/,//'`
 
-    for ITEM in ${UPLOAD_ITEM[@]}
+    for item in ${upload_item[@]}
     do
-        upload $ITEM
+        upload $item
     done
 
     echo "upload completed!"
 
-    cd $SHELL_PATH
+    cd $shell_path
 
     rm -rf result
 fi
